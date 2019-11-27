1c2780a22720f05bdecb4a12a21b8c88203a7bc9
wjlkongxi002 <wjlkongxi@163.com>
2019-11-27 23:47:35 +0800

    类 更新

--- Test_11_24/Test_11_24/Test.cpp
+++ Test_11_24/Test_11_24/Test.cpp
@@ -2,6 +2,52 @@
 #include <iostream>
 using namespace std;
 
+int& Add(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+	int main()
+	{
+		int &ret = Add(1, 2);
+		Add(3, 4);
+		cout << "Add(1,2)=" << ret << endl;
+		return 0;
+	}
+
+
+/*
+void main()
+{
+	//const int &a = 10;
+	////a = 12;不能对常引用变量进行修改
+	//const int &b = 10;
+	//double d = 12.34;
+	////int &rd = d;编译错误，rd为d的引用（别名），可能会更改d的值（而d是一个常量）
+	//const int &rd = d;
+	const int &d = 20;   
+	printf("d = %d\n", d);
+
+
+	int *p = (int*)&d;
+	*p = 40;
+	printf("d = %d\n", d);
+
+
+}
+
+void main()
+{
+	int a = 10;
+	int &ra = a;
+	int &rb = a;
+	double b = 12.34;
+	
+	a = 100;
+	cout << a << ra << rb <<rc<< endl;
+}
+
+
 class CGoods
 {
 public:
@@ -67,14 +113,14 @@ void main()
 
 
 
-//void main()
-//{
-//	namespace N
-//	{
-//	}
-//}
+void main()
+{
+	namespace N
+	{
+	}
+}
 
-/*void main()
+void main()
 {
 int a= 10;
 char b= 'b';
