703c50c37d025a29e65e64f8e736aca43e76ef44
wjlkongxi002 <wjlkongxi@163.com>
2020-02-17 14:04:53 +0800

    类与对象

--- 寒假复习/寒假复习/test.cpp
+++ 寒假复习/寒假复习/test.cpp
@@ -1,8 +1,282 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<assert.h>
 using namespace std;
 
+class Date
+{
+public:
+	void Display()
+	{
+		cout << "Display ()" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
+	}
+	void Display() const
+	{
+		cout << "Display () const" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
+	}
+private:
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
+};
+void Test()
+{
+	Date d1;
+	d1.Display();
+	const Date d2;
+	d2.Display();
+}
+int main()
+{
+	Test();
+}
+/*
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	// bool operator==(Date* this, const Date& d2)
+	// 这里需要注意的是，左操作数是this指向的调用函数的对象
+	bool operator==(const Date& d2)
+	{
+		return _year == d2._year
+			&& _month == d2._month
+			&& _day == d2._day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+void Test()
+{
+	Date d1(2018, 9, 26);
+	Date d2(2018, 9, 27);
+	cout << (d1 == d2) << endl;
+}
+int main()
+{
+	Test();
+	return 0;
+}
 
+
+
+/*
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	//private:
+	int _year;
+	int _month;
+	int _day;
+};
+// 这里会发现运算符重载成全局的就需要成员变量是共有的，那么问题来了，封装性如何保证？
+// 这里其实可以用我们后面学习的友元解决，或者干脆重载成成员函数。
+bool operator==(const Date& d1, const Date& d2)
+{
+	return d1._year == d2._year
+	       && d1._month == d2._month
+		&& d1._day == d2._day;
+}
+void Test()
+{
+	Date d1(2018, 9, 26);
+	Date d2(2018, 9, 27);
+	cout << (d1 == d2) << endl;
+}
+int main()
+{
+	Test();
+	return 0;
+}
+
+/*
+class String
+{
+public:
+	String(const char* str = "jack")
+	{
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		cout << "~String()" << endl;
+		free(_str);
+	}
+private:
+	char* _str;
+};
+int main()
+{
+	String s1("hello");
+	String s2(s1);
+}
+
+/*
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1;
+	// 这里d2调用的默认拷贝构造完成拷贝，d2和d1的值也是一样的。
+	Date d2(d1);
+		return 0;
+}
+
+
+/*
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1;
+	Date d2(d1);
+	return 0;
+}
+
+/*
+class String
+{
+public:
+	String(const char* str = "jack")
+	{
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		cout << "~String()" << endl;
+		free(_str);
+	}
+private:
+	char* _str;
+};
+class Person
+{
+private:
+	String _name;
+	int _age;
+};
+int main()
+{
+	Person p;
+	return 0;
+}
+/*
+class Date
+{
+public:
+	Date()
+	{
+		_year = 1900;
+		_month = 1;
+		_day = 1;
+	}
+	/*Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+// 以下测试函数能通过编译吗？
+void Test()
+{
+	Date d1;
+}
+
+int main()
+{
+
+}
+/*
+
+class Date
+{
+public:
+	// 1.无参构造函数
+	Date()
+	{}
+	// 2.带参构造函数
+	Date(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+void TestDate()
+{
+	Date d1; // 调用无参构造函数
+	Date d2(2015, 1, 1); // 调用带参的构造函数
+	// 注意：如果通过无参构造函数创建对象时，对象后面不用跟括号，否则就成了函数声明
+	// 以下代码的函数：声明了d3函数，该函数无参，返回一个日期类型的对象
+	
+}
+
+int main()
+{
+
+}
+
+/*
 int Add(int left, int right)
 {
 	return left + right;
@@ -12,7 +286,7 @@ int main()
 	Add(1, 2);
 	return 0;
 }
-
+*/
 //namespace N
 //{
 //	int a=10;
