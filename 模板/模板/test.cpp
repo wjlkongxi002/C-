e76485ebe6a98a180e48287c2c2ef1cb4dca210d
wjlkongxi002 <wjlkongxi@163.com>
2020-10-23 23:54:49 +0800

    练习

--- 模板/模板/test.cpp
+++ 模板/模板/test.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 //
+/*
 template <class T>  //模板参数列表：声明类型
 T Add(T left, T right)
 {
@@ -19,3 +20,57 @@ int main()
 
 	return 0;
 }
+*/
+
+/*
+template <class T1,class T2>  //模板参数列表：声明类型
+T2 Add(T1 left, T2 right)
+{
+	cout << typeid(left + right).name() << endl;
+	cout << left + right << endl;
+
+	return left + right;
+}
+int main()
+{
+	Add(1, 1.1);
+}
+*/
+//普通函数可以实现自动类型强转
+/*
+ double Add(int a,double b)
+{
+    int c= a + b;
+	cout << c ;
+	return c;
+}
+int main()
+{
+	Add(1, 2.3);  //  3  返回值类型和定义的类型值不一样？
+}
+*/
+
+
+// 模板化各种类型的数组
+template<class T>
+void PrintArray(T array[], size_t size)
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main()
+{
+	int array1[] = { 1, 2, 3, 4, 5 };
+	PrintArray(array1, sizeof(array1) / sizeof(int));
+
+	double array2[] = { 1.1, 2.2, 3.3 };
+	PrintArray(array2, sizeof(array2) / sizeof(double));
+
+	return 0;
+}
+
