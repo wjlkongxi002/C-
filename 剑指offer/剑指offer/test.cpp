2763a07d6e934db8298f0641b78cb2343e832132
wjlkongxi002 <wjlkongxi@163.com>
2020-10-24 19:27:51 +0800

    空格转换

--- 剑指offer/剑指offer/test.cpp
+++ 剑指offer/剑指offer/test.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 
-//1.替换空格
 
-class Solution {
+/*
+Solution {
 public:
 	string replaceSpace(string s) {
 		string s1;
@@ -25,4 +25,28 @@ public:
 int main()
 {
 	string replaceSpace("wang jia");
+}*/
+//1.替换空格
+
+class Solution {
+public:
+	void replaceSpace(char *str, int length) {   //将char* 类型转成string类型，比较好做
+		string str1;
+		for (int i = 0; i <= length; i++)//将str循环写入str1
+		{
+			if (str[i] == ' ')
+				str1 += "%20";
+			else
+				str1 += str[i];
+		}
+		str = (char*)str1.c_str();
+		cout << str << endl;
+	}
+};
+
+int main()
+{
+	Solution s;
+	s.replaceSpace("wang jia",9);
+	return 0;
 }
