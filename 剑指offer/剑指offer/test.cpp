b2458729a7cd786406a9e337a6354caf540a8292
wjlkongxi002 <wjlkongxi@163.com>
2020-10-28 23:51:59 +0800

    和为s的两个数

--- 剑指offer/剑指offer/test.cpp
+++ 剑指offer/剑指offer/test.cpp
@@ -7,6 +7,34 @@
 
 using namespace std;
 
+
+// 剑指 Offer 57. 和为s的两个数字  https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+/*
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {  //双指针
+		int i = 0, j = nums.size() - 1;
+		while (i<j)
+		{
+			int s = nums[i] + nums[j];
+			if (s == target)
+				return{ nums[i], nums[j] };
+			if (s>target)
+				j--;
+			if (s<target)
+				i++;
+		}
+		return{};
+	}
+};
+
+*/
+
+
+
+
+
+
 //1.替换空格  https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
 
 /*
@@ -195,7 +223,12 @@ int main()
 }
 */
 
+// 剑指 Offer 04. 二维数组中的查找
+//在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，
+//每一列都按照从上到下递增的顺序排序。请完成一个函数，
+//输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
+/*
 class Solution {
 public:
 	bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
@@ -215,8 +248,10 @@ public:
 
 int main()
 {
-	Solution s;vector<vector<int> > v = { { 1, 4, 7, 11, 15 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 }, { 18, 21, 23, 26, 30 } };
+	Solution s;
+	vector<vector<int> > v = { { 1, 4, 7, 11, 15 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 }, { 18, 21, 23, 26, 30 } };
 	s.findNumberIn2DArray(v,5);
 	
 	return 0;
 }
+*/
