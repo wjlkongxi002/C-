a6884721d91f673aa480f0ce94f4b0a39b331b3b
wjlkongxi002 <wjlkongxi@163.com>
2020-11-03 23:12:00 +0800

    剑指 倒序打印单链表

--- 剑指offer/剑指offer/test.cpp
+++ 剑指offer/剑指offer/test.cpp
@@ -3,9 +3,37 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-
 using namespace std;
+
+
+// 剑指offer 06 从尾到头打印链表   https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+/*
+
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+
+class Solution {
+public:
+	vector<int> reversePrint(ListNode* head) {
+		vector<int> v;
+		while (head)
+		{
+			v.push_back(head->val);
+			head = head->next;
+		}
+
+		reverse(v.begin(), v.end());
+		return v;
+	}
+};
+
+*/
 
 
 // 剑指 Offer 57. 和为s的两个数字  https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
