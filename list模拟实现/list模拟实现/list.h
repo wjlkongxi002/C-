a71ef325dc189f8b7f8a5bdeb0a1a61d8f7ef216
wjlkongxi002 <wjlkongxi@163.com>
2020-11-16 22:34:46 +0800

    模拟实现部分功能

--- list模拟实现/list模拟实现/list.h
+++ list模拟实现/list模拟实现/list.h
@@ -20,11 +20,11 @@ namespace bite
 	struct ListIterator
 	{
 		typedef ListNode<T> Node;
-		typedef ListIterator<T> Self;
+		typedef ListIterator<T> Self; // list迭代器
 	public:
 		ListIterator(Node* ptr = nullptr)
 			:_ptr(ptr)
-		{}
+		{} // 初始化
 
 		// 具有指针类似的操作
 		T& operator*()
@@ -39,13 +39,13 @@ namespace bite
 
 		//////////////////////
 		// 迭代器要能移动
-		Self& operator++() // 后置++
+		Self& operator++() // 前置++
 		{
 			_ptr = _ptr->next;
 			return *this;
 		}
 
-		Self operator++(int) // 前置++
+		Self operator++(int) // 后置++
 		{
 			Self temp(*this);
 			_ptr = _ptr->next;
@@ -75,15 +75,109 @@ namespace bite
 		{
 			return _ptr == s._ptr;
 		}
-	private:
+
 		Node* _ptr;
 
 	};
 
 
-	template<class T>
-	calss list
+	template<class T, class Iterator>
+	class ListReverseIterator
 	{
+		typedef ListNode<T> Node;
+		typedef ListReverseIterator<T, Iterator> Self;
+	public:
+		ListReverseIterator(Node* pNode = nullptr)
+			: _it(pNode)
+		{}
+
+		ListReverseIterator(Itertor it)
+			:_it(it)
+		{}
+
+		T& operator*()
+		{
+			Itrator temp(_it);
+			--temp;
+			return *temp;
+		}
+
+		T* operator->()
+		{
+			return &(*_it);
+		}
+
+		Self& operator++()
+		{
+			--_it;
+			return *this;
+		}
+
+		Self operator++(int)
+		{
+			_it++;
+			return *this;
+		}
+
+		Self& operator--()
+		{
+			++_it;
+			return *this;
+		}
+
+		Self operator--(int)
+		{
+			_it++;
+			return *this;
+		}
+
+		bool operator != (const Self& s)
+		{
+			return _it != s._it;
+		}
+
+		bool operator == (const Self& s)
+		{
+			return _it == s._it;
+		}
+
+	private:
+		Iterator _it;
+
+	};
+
+
+	template<class T>
+	class list
+	{
+		typedef ListNode<T> Node;
+	public:
+		typedef LisIterator<T> iterator;
+		typedef ListReverseIterator<T, iterator> reverse_iterator;
+	public:
+		list()
+		{
+			CreateHead();
+		}
+
+		list(int n, const T& data)
+		{
+			CreateHead();
+			for (int i = 0; i < n; ++i)
+				push_back(data);
+		}
+
+		template<class Iterator>
+		list(Iterator first, Iterator last)
+		{
+			CreateHead();
+			while (first != last)
+			{
+				push_back(*first);
+				++first;
+			}
+		}
+
 		list(list<T>& L)
 		{
 			CreateHead();
@@ -106,8 +200,107 @@ namespace bite
 		//iterator
 		iterator begin()
 		{
-
+			// 构造一个匿名对象返回了
+			return iterator(head->next);
 		}
+
+		iterator end()
+		{
+			return iterator(head);
+		}
+
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(end());
+		}
+
+		reverse_iterator rend()
+		{
+			return reserve_iterator(begin());
+		}
+
+		/////////////////////////////////////
+		//capacity
+		size_t size()const
+		{
+			size_t count = 0;
+			Node* pCur = head->next;
+			while (pCur != head)
+			{
+				++count;
+				pCur = pCur->next;
+			}
+			return count;
+		}
+		bool empty()
+		{
+			return head->next == head;
+		}
+
+		void resize(size_t newsize, const T& data = T())
+		{
+			size_t oldsize = size();
+			if (newsize <= oldsize)
+			{
+				for (size_t i = newsize; i < oldsize; ++i)
+					pop_back();
+			}
+			else
+			{
+				for (size_t i = oldsize; i < newsize; ++i)
+					push_back(data);
+			}
+		}
+
+		/////////////////////////////////
+		// access
+		T& front()
+		{
+			return *begin();
+		}
+		const T& front()const
+		{
+			return *begin();
+		}
+
+		T& back()
+		{
+			return head->prev->data;
+		}
+		const T& baack()const
+		{
+			return head->prev->data;
+		}
+
+		////////////////////////////////////////
+		void push_front(const T& data)
+		{
+			insert(begin(), data);
+		}
+
+		void pop_front()
+		{
+			erase(begin());
+		}
+
+		void push_back(const T& data)
+		{
+			insert(end(), data);
+		}
+
+		void pop_back()
+		{
+			erase(--end());
+		}
+
+		iterator insert(iterator pos, const T& data)
+		{
+			Node* newnode = new Node(data);
+		
+		}
+
+
+
 
 	private:
 		void CreateHead()
