212441e7f7888e6bd8c274a65031703f67965401
wjlkongxi002 <wjlkongxi@163.com>
2020-11-23 19:16:05 +0800

    查找子串

--- 牛客力扣/牛客力扣/Test.cpp
+++ 牛客力扣/牛客力扣/Test.cpp
@@ -2,7 +2,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
+
+
+// https://www.nowcoder.com/questionTerminal/181a1a71c7574266ad07f9739f791506
+//查找两个字符串a, b中的最长公共子串。若有多个，输出在较短串中最先出现的那个。
+//注：子串的定义：将一个字符串删去前缀和后缀（也可以不删）形成的字符串。请和“子序列”的概念分开！
+
+int main()
+{
+	string a, b;
+	while (cin >> a >> b)
+	{
+		if (a.size()>b.size())
+			swap(a, b);
+		string str_m;
+		for (int i = 0; i<a.size(); i++)
+		{
+			for (int j = i; j<a.size(); j++)
+			{
+				string temp = a.substr(i, j - i + 1);
+				if (int(b.find(temp))<0)
+					break;
+				else if (str_m.size()<temp.size())
+					str_m = temp;
+			}
+		}
+		cout << str_m << endl;
+	}
+	return 0;
+}
+
 
 // 组队竞争
 // https://www.nowcoder.com/questionTerminal/6736cc3ffd1444a4a0057dee89be789b?orderByHotValue=1&page=1&onlyReference=false
