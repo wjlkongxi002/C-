2a0511057c7ddfb1cf916ef11c6975ee66acab13
wjlkongxi002 <wjlkongxi@163.com>
2020-11-25 23:56:20 +0800

    排序子序列

--- 牛客力扣/牛客力扣/Test.cpp
+++ 牛客力扣/牛客力扣/Test.cpp
@@ -5,6 +5,8 @@
 #include <string>
 using namespace std;
 
+
+/*
 class Solution {
 public:
 	int MoreThanHalfNum_Solution(vector<int> numbers) {
@@ -34,9 +36,47 @@ int main()
 }
 
 
+*/
 
-
-
+int main()
+{
+	int n;
+	cin >> n;
+	// 注意这里多给了一个值，是处理越界的情况的比较，具体参考上面的解题思路
+	vector<int> a;
+	a.resize(n + 1);
+	a[n] = 0;
+	//读入数组
+	int i = 0;
+	for (i = 0; i < n; ++i)
+		cin >> a[i];
+	i = 0;
+	int count = 0;
+	while (i < n)
+	{
+		// 非递减子序列
+		if (a[i] < a[i + 1])
+		{
+			while (i < n && a[i] <= a[i + 1])
+				i++;
+			count++;
+			i++;
+		} 
+		else if (a[i] == a[i + 1])
+		{
+			i++;
+		} 
+		else // 非递增子序列
+		{
+			while (i < n && a[i] >= a[i + 1])
+			i++;
+			count++;
+			i++;
+		}
+	}
+	cout << count << endl;
+	return 0;
+}
 
 
 
