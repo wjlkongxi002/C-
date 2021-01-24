96adb24ca0f7077e45acf2e1ec610523a1ce0810
wjlkongxi002 <wjlkongxi@163.com>
2021-01-24 18:19:16 +0800

    每日一题

---
+++ 2021_1_24/2021_1_24/Test.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+// https://www.nowcoder.com/practice/fbcf95ed620f42a88be24eb2cd57ec54?tpId=49&&tqId=29311&rp=1&ru=/activity/oj&qru=/ta/2016test/question-ranking
+// Î¢ÐÅºì°ü
+/*
+class Gift {
+public:
+	int getValue(vector<int> gifts, int n) {
+
+		sort(gifts.begin(), gifts.end());
+		int middle = gifts[n / 2];
+		int count = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			if (gifts[i] == middle)
+				count++;
+		}
+		if (count > n / 2)
+			return middle;
+		return 0;
+
+	}
+};
+
+*/
