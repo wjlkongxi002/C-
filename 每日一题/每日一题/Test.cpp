e240fc0c581486a4044bf991a311bd47972c9f6c
wjlkongxi002 <wjlkongxi@163.com>
2021-02-07 17:29:41 +0800

    每日一题

---
+++ 每日一题/每日一题/Test.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////////////////////////////
+// 1
+// https://www.nowcoder.com/questionTerminal/6736cc3ffd1444a4a0057dee89be789b?orderByHotValue=1&page=1&onlyReference=false
+// ×é¶Ó¾ºÈü
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n;
+	long long sum = 0;
+	while (cin >> n)
+	{
+		vector<int> v;
+		v.resize(3 * n);
+		for (int i = 0; i < 3 * n; ++i)
+		{
+			cin >> v[i];
+		}
+		sort(v.begin(), v.end());
+		for (int j = n; j <= 3 * n - 2; j += 2)
+		{
+			sum += v[j];
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
+
+*/
