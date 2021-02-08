c48a948d5ec8e5b0959e38ba135c81e28e713deb
wjlkongxi002 <wjlkongxi@163.com>
2021-02-08 16:39:07 +0800

    每日一题

--- 每日一题/每日一题/Test.cpp
+++ 每日一题/每日一题/Test.cpp
@@ -31,3 +31,34 @@ int main()
 }
 
 */
+
+////////////////////////////
+//2
+//https://www.nowcoder.com/practice/f0db4c36573d459cae44ac90b90c6212?tpId=85&tqId=29868&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2F2017test%2Fquestion-ranking&tab=answerKey
+//É¾³ý¹«¹²×Ö·û
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string s1, s2;
+	string s;
+	getline(cin, s1);
+	getline(cin, s2);
+	int Hashtable[256] = { 0 };
+	for (int i = 0; i < s2.size(); ++i)
+	{
+		Hashtable[s2[i]]++;
+	}
+
+	for (int j = 0; j < s1.size(); ++j)
+	{
+		if (Hashtable[s1[j]] == 0)
+			s += s1[j];
+	}
+	cout << s << endl;
+	return 0;
+}
+
+*/
